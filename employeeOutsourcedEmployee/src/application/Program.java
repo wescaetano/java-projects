0cc8a8790e0991d94fb220ad4ee00460ac524a20
wescaetano <weslleycaetanosoares@gmail.com>
2025-02-08 19:46:30 -0300

    New polimorfism project

---
+++ employeeOutsourcedEmployee/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        List<Employee> employee = new ArrayList<>(n);
+        for(int i=0; i<n; i++){
+            System.out.println("Employee #" + (i+1) + ": data");
+            System.out.print("Outsourced (y/n): ");
+            char outsourced = sc.next().charAt(0);
+            sc.nextLine();
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            if(outsourced == 'y'){
+                System.out.print("AdditionalCharge: ");
+                double additionalCharge = sc.nextDouble();
+                employee.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+            } else if(outsourced == 'n') {
+                employee.add(new Employee(name, hours, valuePerHour));
+            } else{
+                System.out.println("Error! this input is invalid!");
+            }
+        }
+
+        for(Employee emp : employee){
+            System.out.println(emp);
+        }
+
+        sc.close();
+    }
+}
---
+++ employeeOutsourcedEmployee/src/entities/OutsourcedEmployee.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class OutsourcedEmployee extends Employee {
+    private Double additionalCharge;
+
+    public OutsourcedEmployee(Double additionalCharge) {
+        super();
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+
+    @Override
+    public Double payment(){
+        return  super.payment() + additionalCharge * 1.1;
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString();
+    }
+}
