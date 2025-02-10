e0d1d63f5837254aaa7f16e4b6de31886c496535
wescaetano <weslleycaetanosoares@gmail.com>
2025-02-09 22:36:59 -0300

    Update in output message

--- employeeOutsourcedEmployee/src/application/Program.java
+++ employeeOutsourcedEmployee/src/application/Program.java
@@ -11,7 +11,7 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the number of employees: ");
+        System.out.print("Enter the number of workers: ");
         int n = sc.nextInt();
         sc.nextLine();
 
