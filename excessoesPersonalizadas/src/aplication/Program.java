91af8fa5ad8969e863a882610ff5951132088cce
wescaetano <weslleycaetanosoares@gmail.com>
2025-02-12 16:38:06 -0300

    Very bad solution

---
+++ excessoesPersonalizadas/src/aplication/Program.java
@@ -0,0 +1,46 @@
+package aplication;
+
+import model.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.zip.DeflaterOutputStream;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat fmt1 = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int roomNumber = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Check-in date (dd/MM/yyyy): ");
+        Date checkIn = fmt1.parse(sc.next());
+        System.out.println("Check-out date (dd/MM/yyyy): ");
+        Date checkOut = fmt1.parse(sc.next());
+        if(!checkOut.after(checkIn)){
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reservation reservation = new Reservation(roomNumber,checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+            System.out.println("Enter date to update the reservation: ");
+            System.out.println("Check-in date (dd/MM/yyyy): ");
+            checkIn = fmt1.parse(sc.next());
+            System.out.println("Check-out date (dd/MM/yyyy): ");
+            checkOut = fmt1.parse(sc.next());
+
+            Date now = new Date();
+            if(checkIn.before(now) || checkOut.before(now)){
+                System.out.println("Error in reservation: Reservation dates for updates must be future dates");
+            } else if(!checkOut.after(checkIn)){
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            } else {
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println("Updated reservation: " + reservation);
+            }
+        }
+
+    }
+}
---
+++ excessoesPersonalizadas/src/model/Reservation.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat fmt1 = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // Esta função converte o tempo em milisegundos para dias.
+    }
+
+    public void updateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return  "roomNumber: " + roomNumber +
+                ", checkIn: " + fmt1.format(checkIn) +
+                ", checkOut: " + fmt1.format(checkOut) +
+                ", " + duration() + " nights";
+    }
+}
