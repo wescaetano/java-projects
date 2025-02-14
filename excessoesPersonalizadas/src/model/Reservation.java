f8cb2256866b2ecfb1d501101164df39e0c2f5e4
wescaetano <weslleycaetanosoares@gmail.com>
2025-02-14 11:00:24 -0300

    Bad solution

--- excessoesPersonalizadas/src/model/Reservation.java
+++ excessoesPersonalizadas/src/model/Reservation.java
@@ -41,9 +41,16 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // Esta função converte o tempo em milisegundos para dias.
     }
 
-    public void updateDates(Date checkIn, Date checkOut){
+    public String updateDates(Date checkIn, Date checkOut){
+        Date now = new Date();
+        if(checkIn.before(now) || checkOut.before(now)){
+            return "Reservation dates for updates must be future dates";
+        } else if(!checkOut.after(checkIn)){
+            return "Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
 
     @Override
