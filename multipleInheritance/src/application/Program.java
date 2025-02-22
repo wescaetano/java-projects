01a8ffa611967fca235749c74052a479fceb788c
wescaetano <weslleycaetanosoares@gmail.com>
2025-02-22 14:06:09 -0300

    ComboDevice

--- multipleInheritance/src/application/Program.java
+++ multipleInheritance/src/application/Program.java
@@ -1,16 +1,24 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
     public static void main(String[] args) {
-        Printer p = new Printer("1080");
+        ConcretePrinter p = new ConcretePrinter("1080");
         p.processDoc("My letter");
         p.print("My letter");
 
-        Scanner s = new Scanner("2030");
+        System.out.println();
+        ConcreteScanner s = new ConcreteScanner("2030");
         s.processDoc("My email");
         System.out.println("Scan result: " + s.scan());
+
+        System.out.println();
+        ComboDevice c = new ComboDevice("3020");
+        c.processDoc("My dissertation");
+        c.print("My dissertation");
+        System.out.println("Scan result: " + c.scan());
     }
 }
