84cd772d508c924d1802e564e6d55e4267460d33
wescaetano <weslleycaetanosoares@gmail.com>
2025-02-23 14:20:33 -0300

    New project: defaultMethods

---
+++ defaultMethods/src/entities/UsaInterestService.java
@@ -0,0 +1,17 @@
+package entities;
+
+import java.security.InvalidParameterException;
+
+public class UsaInterestService implements InterestService{
+    private double interestRate;
+
+
+    public UsaInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+}
