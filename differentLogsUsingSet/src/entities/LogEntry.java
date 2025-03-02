ad2d9a10aedfab4a029c003a9d58fa4f47113f21
wescaetano <weslleycaetanosoares@gmail.com>
2025-03-02 12:23:27 -0300

    New project: differentLogsUsingSet

---
+++ differentLogsUsingSet/src/entities/LogEntry.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class LogEntry {
+    private String name;
+    private Date moment;
+
+    public LogEntry() {
+    }
+
+    public LogEntry(String name, Date moment) {
+        this.name = name;
+        this.moment = moment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LogEntry logEntry = (LogEntry) o;
+        return Objects.equals(name, logEntry.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
