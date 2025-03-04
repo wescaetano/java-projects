5365516df573cd21d751f52d14d0d27b571ba002
wescaetano <weslleycaetanosoares@gmail.com>
2025-03-04 16:23:03 -0300

    First version: bad solution

---
+++ lambdaExpression/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv",  1500.00));
+        list.add(new Product("Notebook",  1200.00));
+        list.add(new Product("Tablet",  450.00));
+
+        Collections.sort(list);
+
+        for(Product prod : list){
+            System.out.println(prod);
+        }
+    }
+}
