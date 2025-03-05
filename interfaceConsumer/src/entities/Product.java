960d8f4e615271307a7ecd65b34f8279ec6e7e46
wescaetano <weslleycaetanosoares@gmail.com>
2025-03-05 14:55:04 -0300

    Reference method with non static method

--- interfaceConsumer/src/entities/Product.java
+++ interfaceConsumer/src/entities/Product.java
@@ -32,6 +32,10 @@ public class Product {
         p.setPrice(p.getPrice() * 1.1);
     }
 
+    public void nonStaticPriceUpdate() {
+        price = price * 1.1;
+    }
+
     @Override
     public String toString() {
         return "name: " + name +
